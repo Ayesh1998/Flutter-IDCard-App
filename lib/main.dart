5c50c828c1fc6517df5da50caf27f0e943255b95
Ayesh1998 <ayeshlak1998@gmail.com>
2020-04-26 00:07:23 +0530

    Added email, backgroud image and a avater

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,89 @@ void main() => runApp(MaterialApp(
 class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[500],
-      appBar: AppBar(
-        title: Text("SL ID Card"),
-        centerTitle: true,
-        backgroundColor: Colors.grey[600],
+    return Stack(children: <Widget>[
+      Image.asset(
+        'assets/IMG-aluth.com-049.jpg',
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "Name",
-              style: TextStyle(
-                  color: Colors.grey[900], letterSpacing: 2.0, fontSize: 20),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "Ayesh Lakshan",
-              style: TextStyle(
-                  color: Colors.grey[900],
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(
+            "SL ID Card",
+            style: TextStyle(fontSize: 23),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/IMG-aluth.com-031.jpg'),
+                  radius: 50.0,
+                ),
+              ),
+              Divider(
+                height: 60.0,
+                color: Colors.blue[300],
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Text(
+                "Name",
+                style: TextStyle(
+                    color: Colors.grey[500], letterSpacing: 2.0, fontSize: 20),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "Ayesh Lakshan",
+                style: TextStyle(
+                    color: Colors.grey[300],
+                    letterSpacing: 2.0,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 40.0),
+              Text(
+                "ID",
+                style: TextStyle(
+                    color: Colors.grey[500], letterSpacing: 2.0, fontSize: 20),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "IT18146757",
+                style: TextStyle(
+                    color: Colors.grey[300],
+                    letterSpacing: 2.0,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.blueGrey[100],
+                  ),
+                  SizedBox(width: 20.0),
+                  Text(
+                    "ayeshlak1998@gmail.com",
+                    style: TextStyle(
+                        fontSize: 20, letterSpacing: 1.5, color: Colors.white),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-    );
+    ]);
   }
 }
